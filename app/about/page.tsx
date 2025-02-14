21c3cfa724df58c12c75663eb3b5b1a1d43014ae
Lafiasabigado <lafiasabigado97@gmail.com>
2025-02-14 15:01:00 +0100

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 const About = () => {
   return (
-    <div className='gap-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid lg:grid-cols-2 md:grid-cols-2 items-center pt-5'>
+    <div className='gap-4 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid lg:grid-cols-2 md:grid-cols-2 items-center pt-5'>
       <div className=''>
          <p 
          className='uppercase text-5xl font-bold my-2 py-2'>
@@ -18,25 +18,25 @@ const About = () => {
          </p>
          <div className='flex items-center space-x-4 pt-5'>
             <Button className='bg-blue-400 border rounded-full'>Contact Me</Button>
-            <div className='text-blue-500'>
+            <div className='text-blue-400'>
              <Link href={"https://linkedin.com/in/lafia-sabi-gado-03910a292/"}>
                <Linkedin />
              </Link>
             </div>
-            <div className='text-blue-500'>
+            <div className='text-blue-400'>
               <Link href={"https://github.com/LafiaSabiGado"}>
                 <Github />
               </Link>
             </div>
          </div>
       </div>
-      <div className=''>
+      <div className='rounded-xl flex justify-center items-center w-full h-full border' style={{backgroundColor:"#C7C7C7"}}>
         <Image 
         src='/logo1.png' 
         alt='Lafia' 
-        width={400} 
-        height={400}
-        className='block' 
+        width={500} 
+        height={500}
+        className='' 
         />
       </div>
     </div>

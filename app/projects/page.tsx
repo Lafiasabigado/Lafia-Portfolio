f0323d988664d85ff811e4d125e50d0db629c00d
Lafiasabigado <lafiasabigado97@gmail.com>
2025-02-18 14:15:34 +0100

    projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -42,7 +42,7 @@ export const Projects = () => {
         <hr className='pt-5 text-zinc-700'/>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-5'>
            <h2 className='text-4xl font-bold uppercase'> Featured Projects</h2>
-           <p className='dark:text-gray-300 py-2'>Here are some of the selected projects that showcase my passion for front-end development.</p>
+           <p className='dark:text-gray-300 py-2'>Here are some of the selected projects that showcase my passion for full-stack development.</p>
         </div>
     </div>
   )
